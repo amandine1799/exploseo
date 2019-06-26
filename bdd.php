2d370e9a43799b2f55995131758d85c93b67f661
bologia <gusberti.simplon@gmail.com>
2019-06-26 15:06:13 +0200

    explo name

--- bdd.php
+++ bdd.php
@@ -4,7 +4,7 @@ $username = "root";
 $password = "";
 
 try {
-    $bdd = new PDO("mysql:host=$servername;dbname=cms_exploseo;charset=utf8mb4", $username, $password);
+    $bdd = new PDO("mysql:host=$servername;dbname=cms_explo;charset=utf8mb4", $username, $password);
     // set the PDO error mode to exception
     $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
