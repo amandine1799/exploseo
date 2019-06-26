872bd25da1e094579a58efc0d5bc48c3c0001a76
amandine <gaigniere.amandine@live.fr>
2019-06-26 15:19:20 +0200

    Modif inscrip

--- inscription.php
+++ inscription.php
@@ -5,6 +5,7 @@
 <body>
     <form action="newaccount.php" method="post">
         <input type="hidden" name="id_membres" maxlength="32"/><br/>
+        Level: <input type="text" name="id_level" maxlength="16"/>
         Pseudo: <input type="text" name="pseudo" maxlength="32"/><br/>
         Mot de passe: <input type="password" name="mdp" maxlength="16"/>
         <br />
